964b82c08dcfc16110fc5534840462a55274ff99
stygiansabyss <stygian.warlock.v2@gmail.com>
2016-12-01 14:22:09 -0600

    Updating constants

--- app/Services/Donating/Models/Donation.php
+++ app/Services/Donating/Models/Donation.php
@@ -6,6 +6,10 @@ use App\Models\BaseModel;
 
 class Donation extends BaseModel
 {
+    const CREATED_AT = 'created_at';
+
+    const UPDATED_AT = 'updated_at';
+
     protected $table = 'donations';
 
     protected $connection = 'mysql4mb';
