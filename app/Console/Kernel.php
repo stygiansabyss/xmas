29111348de739ad97738d8deb0a77aed72932b8e
Dylan Akhawais <dylanakhawais@live.co.uk>
2017-01-01 01:09:43 +0000

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        if (in_array(date('m'), ['11', '12'])) {
+        if (in_array(date('m'), ['1', '12'])) {
             $filePath = storage_path('logs/cronlog');
 
             $schedule->command('jj:donations')->everyMinute()->sendOutputTo($filePath .'_donations');
